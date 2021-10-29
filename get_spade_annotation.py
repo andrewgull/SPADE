3978bb16e5ea3ca9b3b93495d9438d569b04f8fc
Andrei Guliaev <andrewgull87@gmail.com>
2021-10-29 15:28:39 +0200

    fix get_spade_annotation()

--- get_spade_annotation.py
+++ get_spade_annotation.py
@@ -5,6 +5,7 @@ import sys
 import codecs
 from Bio import SeqIO
 import argparse
+from glob import glob
 
 
 def get_args():
@@ -26,27 +27,29 @@ def get_args():
 
 def get_spade_annotations(dir_name="."):
     # in the following line I inserted that lonely function argument in order this function could read files
-    dirs = os.listdir(dir_name)
+    # dirs = os.listdir(dir_name)
     # find all SPADE.gb files in the given directory and its subdirs
-    periodic_repeats = [] 
-    spade_gbk_list = [f for f in dirs if "SPADE" in f]  
+    periodic_repeats = list()
+    # spade_gbk_list = [f for f in dirs if "SPADE" in f]
+    spade_gbk_list = glob(dir_name + "/*/*_SPADE.gb")
     for gbk in spade_gbk_list:
-        for record in list(SeqIO.parse(codecs.open(gbk, 'r', 'utf-8', 'ignore'),"genbank")):
-            gbk_id   = record.id
+        for record in list(SeqIO.parse(gbk, "genbank")):
+            gbk_id = record.id
             CDS_list = []  
             for feature in record.features:
                 if feature.type == "repeat_region":
                     if "note" in feature.qualifiers.keys():
                         if "SPADE" in feature.qualifiers["note"][0]:
-                            start, end   = feature.location.start, feature.location.end
-                            period       = feature.qualifiers["period"][0]
-                            data_type    = feature.qualifiers["sequence_type"][0] 
-                            repeat_motif = feature.qualifiers["unmasked_rpt_unit_seq"][0].replace(" ","_").upper()  
-                            rptnum       = feature.qualifiers["rpt_num"][0]  
-                            variable_repeat_motif = feature.qualifiers["rpt_unit_seq"][0].replace(" ","_").upper()  
+                            start, end = feature.location.start, feature.location.end
+                            period = feature.qualifiers["period"][0]
+                            data_type = feature.qualifiers["sequence_type"][0]
+                            repeat_motif = feature.qualifiers["unmasked_rpt_unit_seq"][0].replace(" ", "_").upper()
+                            rptnum = feature.qualifiers["rpt_num"][0]
+                            variable_repeat_motif = feature.qualifiers["rpt_unit_seq"][0].replace(" ", "_").upper()
                             periodicity = feature.qualifiers["periodicity_score"][0]  
 
-                            periodic_repeats.append([gbk_id,str(start),str(end),period,periodicity,rptnum,data_type,repeat_motif,variable_repeat_motif,"N/A"])
+                            periodic_repeats.append([gbk_id, str(start), str(end), period, periodicity, rptnum,
+                                                     data_type, repeat_motif, variable_repeat_motif, "N/A"])
                 if feature.type == "CDS":
                     CDS_list.append(feature) 
            
@@ -60,21 +63,23 @@ def get_spade_annotations(dir_name="."):
     return periodic_repeats
 
 
-def remove_overlap(periodic_repeats): 
-    new_periodic_repeats  = []
-    dna_s_e_list     = []
+def remove_overlap(prd_rpts):
+    """
+    param: prd_rpts - periodic repeats array from get_spade_annotation()
+    """
+    new_periodic_repeats = []
+    dna_s_e_list = []
     protein_s_e_list = []
-    for line in periodic_repeats: 
-        start, end  = int(line[1]), int(line[2]) 
-        seq_type    = line[-4] 
+    for line in prd_rpts:
+        start, end = int(line[1]), int(line[2])
+        seq_type = line[-4]
         if seq_type == "prot":
             protein_s_e_list.append([start, end]) 
             new_periodic_repeats.append(line) 
         else:
-            dna_s_e_list.append([start,end,line]) 
+            dna_s_e_list.append([start, end, line])
 
-
-    for d_set in dna_s_e_list: 
+    for d_set in dna_s_e_list:
         flag = 0
         for p_set in protein_s_e_list:
             if d_set[0] < p_set[1] and d_set[1] > p_set[0]:
@@ -95,15 +100,18 @@ def crispr_search(features, s, e):
         if len(list(set(range(feature.location.start,feature.location.end)) & set(range(s,e)))) > 0:
             if feature.type == "repeat_region" and "rpt_family" in feature.qualifiers.keys():
                 if feature.qualifiers["rpt_family"][0] == "CRISPR":
-                    overlap = (1.0 * min([e,feature.location.end]) - max([s,feature.location.start])) / (max([e,feature.location.end]) - min([s,feature.location.start]))
+                    overlap = (1.0 * min([e, feature.location.end]) - max([s, feature.location.start])) / (max([e, feature.location.end]) - min([s, feature.location.start]))
                     break
     return overlap
 
 
 if __name__ == "__main__":
-    output = open("SPADE_annotations.tsv","w")
-    output.write("\t".join(["NCBI RefSeqID","Start","End","Period","Periodicity","#Repeat unit","Sequence type","Repetitive motif","Repetitive motif masked variable positions","Overlapped CDS annotations"]) + "\n")
-    for record in SeqIO.parse(sys.argv[1], "genbank"):
+    args = get_args()
+    output = open(args.output + "/SPADE_annotations.tsv", "w")
+    output.write("\t".join(["NCBI RefSeqID", "Start", "End", "Period", "Periodicity", "#Repeat unit", "Sequence type",
+                            "Repetitive motif", "Repetitive motif masked variable positions",
+                            "Overlapped CDS annotations"]) + "\n")
+    for record in SeqIO.parse(args.input_dir, "genbank"):
         periodic_repeats = get_spade_annotations()
         periodic_repeats = remove_overlap(periodic_repeats) 
         for row in periodic_repeats:
